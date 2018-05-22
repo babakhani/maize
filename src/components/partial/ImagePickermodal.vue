e1834e13b5d3ecee96ce2ba4c94126e512810b16
bitaedalati <bita.edalati@gmail.com>
2018-05-22 18:16:12 +0430

    add ImagePickerModal.vue

---
+++ src/components/partial/ImagePickermodal.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-modal
+    @hidden="onHide"
+    @ok="onOk"
+    v-model.sync="showModal"
+    id="imagePickerModal"
+    size="lg"
+    centered
+    class="image-picker-modal"
+    title="Choose your pick:">
+
+    <div>
+      <img :class="{'image-picker-modal-selected' : pick }" class="image-picker-modal--img"  v-for="imageItem in fakeImagesForTest" @click="pick(imageItem)"
+         :src="imageItem">
+    </div>
+
+  </b-modal>
+</template>
+
+<script>
+  import {EventBus} from '../../events/event-bus'
+  export default {
+    name: 'ImagePickerModal',
+    methods: {
+      onHide() {
+        this.$store.dispatch('main/setPickImageMode', false)
+      },
+      onOk() {
+//        // TODO: complete this
+//        this.$store.dispatch('main/addToCurrentWidgetList', this.addWidgetList)
+        if (this.pickedImageSrc) {
+          EventBus.$emit('pickImage', this.pickedImageSrc)
+        }
+
+      },
+      pick(imageSrc) {
+        this.pickedImageSrc = imageSrc
+      }
+    },
+    computed: {
+      modalShowGlobalState() {
+        return this.$store.state.main.pickImageMode
+      }
+    },
+    data() {
+      return {
+        showModal: false,
+        pickedImageSrc: null,
+        fakeImagesForTest: [
+          'https://source.unsplash.com/user/erondu/160x90',
+          'https://source.unsplash.com/collection/190727/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/user/erondu/160x90',
+          'https://source.unsplash.com/collection/190727/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/1600x900/?nature,water/160x90',
+          'https://source.unsplash.com/user/erondu/160x90',
+          'https://source.unsplash.com/collection/190727/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/1600x900/?nature,water/160x90',
+          'https://source.unsplash.com/1600x900/?nature,water/160x90',
+          'https://source.unsplash.com/user/erondu/160x90',
+          'https://source.unsplash.com/collection/190727/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/collection/190737/160x90',
+          'https://source.unsplash.com/1600x900/?nature,water/160x90',
+        ]
+      }
+    },
+    watch: {
+      modalShowGlobalState() {
+        this.showModal = this.modalShowGlobalState
+      }
+    }
+  }
+</script>
