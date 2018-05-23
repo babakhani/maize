764c099b2964c34bebda0e0b7e04a72004af706e
bitaedalati <bita.edalati@gmail.com>
2018-05-23 18:44:01 +0430

    add tabs in modal

--- src/components/partial/ImagePickermodal.vue
+++ src/components/partial/ImagePickermodal.vue
@@ -7,14 +7,41 @@
     centered
     class="image-picker-modal"
     title="Choose your pick:">
-    <div class="image-picker-modal--body ">
-      <img :class="{'image-picker-modal-selected' : pickedImageSrc == imageItem }"
-           class="image-picker-modal--img"
-           v-for="imageItem in fakeImagesForTest"
-           @click="pick(imageItem)"
-           @dblclick="pickAndHide(imageItem)"
-           :src="imageItem">
+    <!--Start Modal Tab-->
+    <nav>
+      <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#" role="tab"
+           aria-controls="nav-home" aria-selected="true">Home</a>
+        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#" role="tab"
+           aria-controls="nav-profile" aria-selected="false">Profile</a>
+        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#" role="tab"
+           aria-controls="nav-contact" aria-selected="false">Contact</a>
+      </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+        <div class="image-picker-modal--body ">
+          <img :class="{'image-picker-modal-selected' : pickedImageSrc == imageItem }"
+               class="image-picker-modal--img"
+               v-for="imageItem in fakeImagesForTest"
+               @click="pick(imageItem)"
+               @dblclick="pickAndHide(imageItem)"
+               :src="imageItem">
+        </div>
+      </div>
+      <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+        insert URL
+      </div>
+      <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">3</div>
     </div>
+    <!--<div class="image-picker-modal&#45;&#45;body ">-->
+    <!--<img :class="{'image-picker-modal-selected' : pickedImageSrc == imageItem }"-->
+    <!--class="image-picker-modal&#45;&#45;img"-->
+    <!--v-for="imageItem in fakeImagesForTest"-->
+    <!--@click="pick(imageItem)"-->
+    <!--@dblclick="pickAndHide(imageItem)"-->
+    <!--:src="imageItem">-->
+    <!--</div>-->
   </b-modal>
 </template>
 
