9b67ed1edb40e273841dc1447f64d293552ee7de
bitaedalati <bita.edalati@gmail.com>
2018-05-28 16:43:32 +0430

    add loading for video request

--- src/components/services/TamashaVideoPicker.vue
+++ src/components/services/TamashaVideoPicker.vue
@@ -1,45 +1,62 @@
 <template>
   <div>
     <div class="col-12 text-center mb-4 image-picker-modal--search-box">
-        <!--TODO: i18n-->
-        <b-form-input @input="loadSearchResult"
-                      type="text"
-                      placeholder="Search video here">
-
-        </b-form-input>
-      </div>
-    <div class="row">
-
+      <!--TODO: i18n-->
+      <b-form-input @input="loadSearchResult"
+                    type="text"
+                    placeholder="Search video here">
+      </b-form-input>
     </div>
-    <div class="image-picker-modal--body">
-      <div class="row">
-        <div class="col-6 col-sm-6 col-md-3 col-xl-3 h-100 image-picker-modal--image-container"
-             v-for="videoItem in videoList">
-          <div class="image-picker-modal--image-thumb-box"
-               @click="pick(videoItem)"
-               @dblclick="pickAndHide(videoItem)">
-            <img
-              class="image-picker-modal--img p-2"
-              :src="videoItem.thumbnailUrl">
+    <div class="row">
+      <div class="col-12 modal-video-picker--box">
+        <div v-if="videoLoading" class="video-loading">
+
+            <h1 class="video-loading--text">loading...</h1>
+
+        </div>
+        <div class="image-picker-modal--body p-3">
+          <div class="row">
+            <div class="col-6 col-sm-6 col-md-3 col-xl-3 h-100 image-picker-modal--image-container"
+
+                 v-for="videoItem in videoList">
+              <div class="image-picker-modal--image-thumb-box"
+
+                   @click="pick(videoItem)"
+                   @dblclick="pickAndHide(videoItem)">
+                <img
+                  class="image-picker-modal--img p-2"
+                  :src="videoItem.thumbnailUrl">
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
     </div>
+    <!--<div class="row">-->
+
+    <!--</div>-->
+
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import Icon from "../../../node_modules/vue-awesome/components/Icon";
 
   export default {
+    components: {Icon},
     name: 'TeamWidget1',
     data () {
       return {
+        videoLoading: false,
         videoList: []
       }
     },
     mounted () {
+
       this.loadSearchResult('z')
+
     },
     props: {
       sourceType: {
@@ -61,9 +78,11 @@
         this.$emit('pickAndHide', this.getData(payload))
       },
       loadSearchResult (query) {
+          this.videoLoading = true
         axios.get(`http://nightlyapi.tamasha.com/api/v1/videos?query=${query}`)
           .then((response) => {
             this.videoList = response.data.data
+            this.videoLoading = false
           })
           .catch(function (error) {
           });
