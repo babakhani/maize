179bc8ec7369214f3b67acc72e83d7a2d547fc37
bita <bita.edalati@gmail.com>
2019-12-12 22:06:11 +0330

    fix: fix addWidget ui

--- src/components/partial/AddWidget.vue
+++ src/components/partial/AddWidget.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="add-widget-box w-100">
+  <div class="add-widget-box w-100 mx-0">
     <!--TODO: bita fix class name-->
-    <div class="add-widget-box--main container">
-      <div class="add-widget-box--main--details">
-        <div class="center-center">
-          <button @click="goToAddWidgetMode"
-                  class="btn btn-add-widget">
-            +
-          </button>
-          <p class="text-add-widget">
-            {{ $t('add_widget') }}
-          </p>
-        </div>
-      </div>
-      <!-- Modal Component -->
-      <b-modal id="modal1"
-               title="Bootstrap-Vue">
-        <p class="my-4">Hello from modal!</p>
-      </b-modal>
-    </div>
+    <b-row class="justify-content-center">
+      <b-col
+         cols="12"
+         md="6"
+         lg="4">
+        <b-card class="add-widget-box--main container">
+          <div class="add-widget-box--main--details">
+            <div class="center-center">
+              <button @click="goToAddWidgetMode"
+                 class="btn btn-add-widget">
+                <icon name="plus"></icon>
+              </button>
+              <p class="text-add-widget">
+              {{ $t('add_widget') }}
+              </p>
+            </div>
+          </div>
+          <!-- Modal Component -->
+          <b-modal id="modal1"
+            title="Bootstrap-Vue">
+            <p class="my-4">Hello from modal!</p>
+          </b-modal>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
