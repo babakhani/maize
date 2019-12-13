33e2127901c1539508f7e62fd1be040fcc3f257b
reza babakhani <babakhani.reza@gmail.com>
2019-12-13 03:46:40 +0330

    chore: upgrade widget calltocation_3

--- src/components/widgets/CallToAction/CallToAction_3.vue
+++ src/components/widgets/CallToAction/CallToAction_3.vue
@@ -1,53 +1,29 @@
 <template>
-  <div v-if="touchedData !== {}"
-       :class="{'container': touchedData.config && !touchedData.config.fullWidth}">
-    <div v-if="demoMode">
+  <div>
+    <WidgetToolbox/>
+    <BgEditable >
       <section class="fdb-block">
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-12 col-sm-8 col-md-6 col-lg-4 text-center">
-              <h3>
-                <strong>Right at the coast of the Semantics, a large language ocean. A small river named Duden.</strong>
-              </h3>
+              <TextEditable 
+                tag="h3"
+                name="mainTitle"
+                :partData="touchedData.mainTitle">
+              </TextEditable>
               <p class="mt-5 mt-sm-4">
-                <a class="btn" href="/">Download</a>
+                <text-editable 
+                  tag="a"
+                  cssClass="btn"
+                  name="action_link"
+                  :partData="touchedData.action_link">
+                </text-editable>
               </p>
             </div>
           </div>
         </div>
       </section>
-    </div>
-    <div v-else
-         class="widget-block">
-      <WidgetToolbox></WidgetToolbox>
-      <div class="widget-block--name">
-        {{$options.name}}
-      </div>
-      <BgEditable
-        name="bg"
-        v-if="touchedData.bg"
-        :partData="touchedData.bg">
-        <section class="fdb-block">
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-12 col-sm-8 col-md-6 col-lg-4 text-center">
-                <text-editable tag="h3"
-                               name="mainTitle"
-                               :partData="touchedData.mainTitle">
-                </text-editable>
-                <p class="mt-5 mt-sm-4">
-                  <text-editable tag="a"
-                                 cssClass="btn"
-                                 name="action_link"
-                                 :partData="touchedData.action_link">
-                  </text-editable>
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </BgEditable>
-    </div>
+    </BgEditable>
   </div>
 </template>
 
