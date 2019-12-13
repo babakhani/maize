0b4dc6b8f0c50f865d6d8c704be02a203130b92d
fariborzemami <fariborz.emami1997@gmail.com>
2019-12-13 16:29:39 +0330

    change naming and add canonical field

--- src/components/partial/ModalSettings.vue
+++ src/components/partial/ModalSettings.vue
@@ -8,7 +8,6 @@
     class=" add-widget-box"
     id="settings-padding"
     title="Site Settings">
-    <!-- Default form contact -->
     <form class="p-3" action="#!">
       <p class="d-block m-1 mb-2 h6">Tittle</p>
       <input type="text" id="tittle" class="d-inline form-control mb-3"
@@ -19,7 +18,8 @@
       placeholder="An awesome landing page">
 
       <p class="d-block m-1 mb-2 h6">Type</p>
-      <input type="text" id="type" class="d-inline form-control mb-3" placeholder="e.g. Splash page">
+      <input type="text" id="type" class="d-inline form-control mb-3" 
+      placeholder="e.g. Splash page">
 
       <p class="d-block m-1 mb-2 h6">Twitter account</p>
       <input type="text" id="twitter" class="d-inline form-control mb-3"
@@ -28,6 +28,10 @@
       <p class="d-block m-1 mb-2 h6">Instagram account</p>
       <input type="text" id="instagram" class="d-inline form-control mb-3"
       placeholder="@Example">
+
+      <p class="d-block m-1 mb-2 h6">Canonical</p>
+      <input type="text" id="canonical" class="d-inline form-control mb-3"
+      placeholder="Canonical">
 
       <p class="d-block m-1 mb-2 h6">Logo url</p>
       <input type="text" id="logo" class="d-inline form-control mb-3"
@@ -38,7 +42,7 @@
       placeholder="http://www.example.com">
 
       <p class="d-block m-1 mb-2 h6">Brand color</p>
-      <input type="color" id="colorPicker" name="brandColor" class="p-0 w-5 form-control mb-3"
+      <input type="color" id="colorPicker" class="p-0 w-5 form-control mb-3"
       placeholder="Choose a color">
     </form>
   </b-modal>
