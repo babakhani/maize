eb0c4255524a3291d3f080233341c8c6b2550aa0
reza babakhani <babakhani.reza@gmail.com>
2019-12-29 02:27:53 +0330

    chore: viewport meta tag

--- src/service/head.js
+++ src/service/head.js
@@ -1,6 +1,7 @@
 export default (settings) => { return `
   <title> ${settings.title} </title>
   <meta name="description" content="${settings.description}"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
