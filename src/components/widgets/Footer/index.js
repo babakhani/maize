bf49e9e09dc41d5a724def5b4f7f8d58ce468b0b
fariborzemami <fariborz.emami1997@gmail.com>
2019-12-31 18:33:10 +0330

    Update "index.js" & "spec.js"

--- src/components/widgets/Footer/index.js
+++ src/components/widgets/Footer/index.js
@@ -3,11 +3,21 @@ import Footer_2 from './Footer_2'
 import Footer_3 from './Footer_3'
 import Footer_4 from './Footer_4'
 import Footer_5 from './Footer_5'
+import Footer_6 from './Footer_6'
+import Footer_7 from './Footer_7'
+import Footer_8 from './Footer_8'
+import Footer_9 from './Footer_9'
+import Footer_10 from './Footer_10'
 
 export default {
   Footer_1: Footer_1,
   Footer_2: Footer_2,
   Footer_3: Footer_3,
   Footer_4: Footer_4,
-  Footer_5: Footer_5
+  Footer_5: Footer_5,
+  Footer_6: Footer_6,
+  Footer_7: Footer_7,
+  Footer_8: Footer_8,
+  Footer_9: Footer_9,
+  Footer_10: Footer_10
 }
