78ccce6e7970015910a71062367c7ee32e0b66d1
reza babakhani <babakhani.reza@gmail.com>
2020-01-15 23:24:54 +0330

    fix: add widget style

--- src/components/partial/AddWidget.vue
+++ src/components/partial/AddWidget.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="add-widget-box w-100 mx-0">
-    <b-row class="justify-content-center add-widget-box--card">
+  <div class="w-100 mx-0 my-5">
+    <b-row class="justify-content-center">
       <b-col
         cols="12"
         md="6"
@@ -36,72 +36,59 @@ export default {
 }
 </script>
 
-<style lang="scss" >
-.add-widget-box {
+<style scoped lang="scss" >
+.add-widget-box--main {
+  border-radius: 30px;
+  width: 20em;
+  height: 20em;
+  cursor: pointer;
+  background: rgba($primary-color, 0.03);
+  position: relative;
+  border: 0;
 
-  margin: 1em;
-
-  .add-widget-box--card {
-    margin-bottom: 5.5em;
+  &:hover {
+    background: rgba($primary-color, 0.07);
   }
 
-  .editor-page-preview-mode & {
-    display: none;
-  }
-
-  .add-widget-box--main {
-    border-radius: 30px;
-    width: 20em;
-    height: 20em;
-    cursor: pointer;
-    background: rgba($primary-color, 0.03);
-    position: relative;
-    border: 0;
-
-    &:hover {
-      background: rgba($primary-color, 0.07);
+  .add-widget-box--main--details {
+    text-align: center;
+    top: 2em;
+    left: 2em;
+    right: 2em;
+    bottom: 2em;
+    position: absolute;
+    .center-center {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
     }
 
-    .add-widget-box--main--details {
-      text-align: center;
-      top: 2em;
-      left: 2em;
-      right: 2em;
-      bottom: 2em;
-      position: absolute;
-      .center-center {
+    .btn-add-widget {
+      position: relative;
+      cursor: pointer;
+      width: 1.5em;
+      height: 1.5em;
+      border: 2px solid $primary-color;
+      border-radius: 100%;
+      box-shadow: 0 2px 4px 0 rgba($primary-color, 0.5);
+      font-size: 5em;
+      &:hover {
+        border-color: $primary-color;
+      }
+      .fa-icon {
         position: absolute;
         top: 50%;
         left: 50%;
-        transform: translate(-50%, -50%);
+        transform: translate(-50%, -50% );
+        color: $primary-color;
       }
-
-      .btn-add-widget {
-        position: relative;
-        cursor: pointer;
-        width: 1.5em;
-        height: 1.5em;
-        border: 2px solid $primary-color;
-        border-radius: 100%;
-        box-shadow: 0 2px 4px 0 rgba($primary-color, 0.5);
-        font-size: 5em;
-        &:hover {
-          border-color: $primary-color;
-        }
-        .fa-icon {
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50% );
-          color: $primary-color;
-        }
-      }
-      .text-add-widget {
-        font-size: 16px;
-        border-color: $primary-color;
-        margin-top: 15px;
-        font-weight: normal;
-      }
+    }
+    .text-add-widget {
+      font-size: 16px;
+      border-color: $primary-color;
+      margin-top: 15px;
+      font-weight: normal;
     }
   }
 }
