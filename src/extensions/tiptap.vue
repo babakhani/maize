1c2f05f7ec0e930d260cc7cfad2389dd168770ba
bita <bita.edalati@gmail.com>
2020-02-01 11:04:11 +0330

    chore: works on tiptap modal

--- src/extensions/tiptap.vue
+++ src/extensions/tiptap.vue
@@ -3,127 +3,111 @@
     <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
       <div class="menubar mb-3">
         <button
-          class="menubar__button btn btn-outline-secondary mr-1 mb-1"
-          :class="{ 'is-active': isActive.bold() }"
-          @click="commands.bold"
-        >
+           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
+           :class="{ 'is-active': isActive.bold() }"
+           @click="commands.bold">
           <icon name="bold" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.italic() }"
-          @click="commands.italic"
-        >
+          @click="commands.italic">
           <icon name="italic" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.strike() }"
-          @click="commands.strike"
-        >
+          @click="commands.strike">
           <icon name="strikethrough" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.underline() }"
-          @click="commands.underline"
-        >
+          @click="commands.underline">
           <icon name="underline" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.code() }"
-          @click="commands.code"
-        >
+          @click="commands.code">
           <icon name="code" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.paragraph() }"
-          @click="commands.paragraph"
-        >
+          @click="commands.paragraph">
           <icon name="paragraph" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-          @click="commands.heading({ level: 1 })"
-        >
+          @click="commands.heading({ level: 1 })">
           H1
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-          @click="commands.heading({ level: 2 })"
-        >
+          @click="commands.heading({ level: 2 })">
           H2
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-          @click="commands.heading({ level: 3 })"
-        >
+          @click="commands.heading({ level: 3 })">
           H3
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.bullet_list() }"
-          @click="commands.bullet_list"
-        >
+          @click="commands.bullet_list">
           <icon name="list-ul" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.ordered_list() }"
-          @click="commands.ordered_list"
-        >
+          @click="commands.ordered_list">
           <icon name="list-ol" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.blockquote() }"
-          @click="commands.blockquote"
-        >
+          @click="commands.blockquote">
           <icon name="quote-right" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
           :class="{ 'is-active': isActive.code_block() }"
-          @click="commands.code_block"
-        >
+          @click="commands.code_block">
           <icon name="code" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
-          @click="commands.horizontal_rule"
-        >
+          @click="commands.horizontal_rule">
           <icon name="minus" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
-          @click="commands.undo"
-        >
+          @click="commands.undo">
           <icon name="undo" />
         </button>
 
         <button
           class="menubar__button btn btn-outline-secondary mr-1 mb-1"
-          @click="commands.redo"
-        >
+          @click="commands.redo">
           <icon name="redo" />
         </button>
       </div>
@@ -212,10 +196,8 @@ export default {
 <style lang="scss">
 .editor {
   .menubar {
-    @media (max-width: 768px) {
-      white-space: nowrap;
-      overflow-x: auto;
-    }
+    white-space: nowrap;
+    overflow-x: auto;
     .menubar__button {
       width: 42px;
       height: 42px;
@@ -225,13 +207,8 @@ export default {
   .editor__content {
     .ProseMirror {
       padding: 1em;
-      min-height: 590px;
-      @media (max-width: 1440px) {
-        min-height: 500px;
-      }
-      @media (max-width: 768px) {
-        min-height: 550px;
-      }
+      min-height: 650px;
+      border: 1px solid;
     }
   }
 }
